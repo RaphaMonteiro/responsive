02fbade2129bd06d3328f9ad6e3ecb1f0411f9c4
raphaelf <raphaelferreiramonteiro@gmail.com>
2022-11-20 22:02:27 -0300

    Alert apenas no homepage

--- scripts.js
+++ scripts.js
@@ -1,45 +1,48 @@
 
-const data= new Date()
-var dia = data.getDate()
-var h = data.getHours()
-var nome_completo = 'Sou o Raphael Ferreira Monteiro'
-var f = data.getDay()
-var curso = 'Sp/ADS'
-var br='\n'
+function cumprimento(){
+    const data= new Date()
+    var dia = data.getDate()
+    var h = data.getHours()
+    var nome_completo = 'Sou o Raphael Ferreira Monteiro'
+    var f = data.getDay()
+    var curso = 'Sp/ADS'
+    var br='\n'
 
-if (h>=6 && h<=11){
+    if (h>=6 && h<=11){
     var buenos = 'Bom Dia!'
-}
-if (h>=12 && h<=17){
+    }
+    if (h>=12 && h<=17){
     var buenos = 'Boa Tarde!'
-}
-if (h>=18 || h<=5){
+    }
+    if (h>=18 || h<=5){
     var buenos = 'Boa Noite!'
-}
+    }
 
-if (f==0){
+    if (f==0){
     var feira = ' Hoje é domingo'
-}
-if (f==1){
+    }
+    if (f==1){
     var feira = ' Hoje é segunda-feira'
-}
-if (f==2){
+    }
+    if (f==2){
     var feira = ' Hoje é terça-feira'
-}
-if (f==3){
+    }
+    if (f==3){
     var feira = ' Hoje é quarta-feira'
-}
-if (f==4){
+    }
+    if (f==4){
     var feira = ' Hoje é quinta-feira'
-}
-if (f==5){
+    }
+    if (f==5){
     var feira = ' Hoje é sexta-feira'
-}
-if (f==6){
+    }
+    if (f==6){
     var feira = ' Hoje é sábado'
-}
+    }
 
 
 
-window.alert(nome_completo + br + curso + br + buenos + feira)
+    window.alert(nome_completo + br + curso + br + buenos + feira)
+    }
+document.getElementById(mensagem).onload = cumprimento()
 
